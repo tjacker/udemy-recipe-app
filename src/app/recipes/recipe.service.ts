f9bfaadd512a1f9699c15dc3d58995657f3813a6
Tim Acker <tjacker31@gmail.com>
2019-02-22 23:28:49 -0500

    Adds ingredients to recipe service and detail view

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,20 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test of the model',
-      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'
+      'Spare Ribs',
+      'Dry rub spare ribs with salad',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+      [
+        new Ingredient('Pork ribs', 1),
+        new Ingredient('Dry rub spices', 1),
+        new Ingredient('Tomatoes', 2)
+      ]
     )
   ];
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
   ingredientsChange = new EventEmitter<Ingredient[]>();
-  private ingredients: Ingredient[] = [new Ingredient('onions', 5), new Ingredient('peppers', 10)];
+  private ingredients: Ingredient[] = [new Ingredient('Onions', 5), new Ingredient('Peppers', 10)];
 
   getIngredients() {
     // Returns a copy of the original array to avoid mutation
