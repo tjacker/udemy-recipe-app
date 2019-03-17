274b2644360d19b23cb05beb1a260222b75e6c18
Tim Acker <tjacker31@gmail.com>
2019-03-16 21:14:20 -0400

    Replaces href navigation links with routerLink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  @Output() linkSelected = new EventEmitter<string>();
-
-  constructor() {}
-
-  onSelect(link: string) {
-    this.linkSelected.emit(link);
-  }
-}
+export class HeaderComponent {}
