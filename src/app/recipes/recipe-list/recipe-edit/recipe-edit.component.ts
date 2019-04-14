226c67cb59230bf045e1c63c8ad899481f78eb3d
Tim Acker <tjacker31@gmail.com>
2019-04-14 16:14:10 -0400

    Navigates up one level when save or cancel buttons are clicked in edit or new recipe mode

--- src/app/recipes/recipe-list/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-list/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 import { RecipeService } from '../../recipe.service';
-import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -15,7 +14,11 @@ export class RecipeEditComponent implements OnInit {
   editMode: boolean = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
@@ -31,6 +34,8 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+    this.onCancel();
   }
 
   getControls() {
@@ -44,6 +49,10 @@ export class RecipeEditComponent implements OnInit {
         amount: new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     );
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   private initForm() {
