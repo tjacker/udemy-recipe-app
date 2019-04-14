78344d053e0310de58b0619d1db139b9edc1d09b
Tim Acker <tjacker31@gmail.com>
2019-04-14 16:37:29 -0400

    Binds recipe image path to preview image source in edit/new recipe mode

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -25,6 +25,12 @@ export class RecipeService {
       'Slow cooker beef stew',
       'https://www.simplyrecipes.com/wp-content/uploads/2013/03/slow-cooker-guinness-stew-horiz-a-1200.jpg',
       [new Ingredient('Stew beef', 1), new Ingredient('Potatoes', 3), new Ingredient('Carrots', 2)]
+    ),
+    new Recipe(
+      'Pad Thai',
+      'Noodles, egg, veggies, and bean sprouts',
+      'https://www.simplyrecipes.com/wp-content/uploads/2019/03/Shrimp_Pad_Thai_HERO00003-1024x682.jpg',
+      [new Ingredient('Noodles', 16), new Ingredient('Eggs', 3), new Ingredient('Shrimp', 10)]
     )
   ];
 
