1e2db5798b6e334e56bc533ee742ab25e3e2328a
Tim Acker <tjacker31@gmail.com>
2019-06-30 21:19:49 -0400

    Updates header navigation based on authentication status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { DataService } from '../shared/data.service';
 import { AuthService } from '../auth/auth.service';
@@ -9,12 +10,21 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSubscription: Subscription;
+  isAuthenticated = false;
+
   constructor(
     private dataService: DataService,
     private authService: AuthService,
     private router: Router
   ) {}
+
+  ngOnInit() {
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataService.storeRecipes();
@@ -27,5 +37,9 @@ export class HeaderComponent {
   onLogout() {
     this.router.navigate(['/signin']);
     this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 }
