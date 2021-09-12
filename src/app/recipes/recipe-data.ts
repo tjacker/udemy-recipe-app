2d4a003bead605e2934cb2417510e0c4babcb716
Tim Acker <tjacker31@gmail.com>
2021-09-12 16:29:11 -0400

    Updates URL links to images in static data

--- src/app/recipes/recipe-data.ts
+++ src/app/recipes/recipe-data.ts
@@ -1,11 +1,11 @@
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
-const recipes: Recipe[] = [
+export const recipes: Recipe[] = [
   new Recipe(
     'Spare Ribs',
     'Dry rub spare ribs with salad',
-    'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+    'https://www.simplyrecipes.com/thmb/Z1nIwZ6oMIwNcHZ8V0yZtzsXM6w=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2011__08__memphis-pork-ribs-horiz-a-1800-62bb309c1649417481d4e1a65ecb11ee.jpg',
     [
       new Ingredient('Pork ribs', 1),
       new Ingredient('Dry rub spices', 1),
@@ -15,13 +15,13 @@ const recipes: Recipe[] = [
   new Recipe(
     'Beef Stew',
     'Slow cooker beef stew',
-    'https://www.simplyrecipes.com/wp-content/uploads/2013/03/slow-cooker-guinness-stew-horiz-a-1200.jpg',
+    'https://www.simplyrecipes.com/thmb/2Sm1eab6gpITB-JNORH6t6TIUkQ=/2000x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2017__11__2017-11-29-Pressure-Cooker-Beef-Stew-15-b06dff2cb17948c1ad61d96d6909e108.jpg',
     [new Ingredient('Stew beef', 1), new Ingredient('Potatoes', 3), new Ingredient('Carrots', 2)]
   ),
   new Recipe(
     'Pad Thai',
     'Noodles, egg, veggies, and bean sprouts',
-    'https://www.simplyrecipes.com/wp-content/uploads/2019/03/Shrimp_Pad_Thai_HERO00003-1024x682.jpg',
+    'https://www.simplyrecipes.com/thmb/cfr1gZnbSxDL_fY8y5OpL3eFu6g=/2000x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__03__Shrimp_Pad_Thai_HERO00003-16e8ac51adc94dd58620c8fa2bdf5f70.jpg',
     [new Ingredient('Noodles', 16), new Ingredient('Eggs', 3), new Ingredient('Shrimp', 10)]
   )
 ];
